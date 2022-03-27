a21132c54991076bff1140bd59f1ab96a294c53d
Hiroto-Iizuka <oroxith@gmail.com>
2022-03-27 21:14:38 +0900

    Providerの記述箇所をApp.jsxに変更

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,26 @@ import { Basic } from "./Basic";
 import { Question } from './Question';
 import { CounselingDetail } from './CounselingDetail';
 import { Confirmation } from './Confirmation';
+import { Provider } from 'react-redux';
+import createStore from './createStore';
+
+export const store = createStore();
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-      </div>
-      <Routes>
-        <Route path="/" element={<Basic />} />
-        <Route path="/question" element={<Question />} />
-        <Route path="/counselingdetail" element={<CounselingDetail />} />
-        <Route path="/confirmation" element={<Confirmation />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="App">
+        </div>
+        <Routes>
+          <Route path="/" element={<Basic />} />
+          <Route path="/question" element={<Question />} />
+          <Route path="/counselingdetail" element={<CounselingDetail />} />
+          <Route path="/confirmation" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
