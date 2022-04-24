c088d8b43d5605671cb86bfd2b33f21ea40e7169
Hiroto-Iizuka <oroxith@gmail.com>
2022-04-24 12:23:13 +0900

    Genderのラジオボタン関連の記述修正*

--- src/components/Gender.jsx
+++ src/components/Gender.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 
 export const Gender = () => {
-  const [radioButton, setRadioButton] = useState("male");
+  const [gender, setGender] = useState("男性");
 
   const handleChange = (e) => {
-    setRadioButton(e.target.value);
+    setGender(e.target.value);
   }
 
   return (
     <div>
       <p>-性別-</p>
       <label>
-        <input type="radio" value="male" onChange={handleChange} checked={radioButton === "male"} />
+        <input type="radio" value="男性" onChange={handleChange} checked={gender === "男性"} />
         男性
       </label>
       <label>
-        <input type="radio" value="female" onChange={handleChange} checked={radioButton === "female"} />
+        <input type="radio" value="女性" onChange={handleChange} checked={gender === "女性"} />
         女性
       </label>
     </div>
