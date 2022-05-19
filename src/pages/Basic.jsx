1c8983c4a25ef0d5b8315d0b9e8780ab1a283805
Hiroto-Iizuka <oroxith@gmail.com>
2022-05-19 23:41:19 +0900

    値を更新するreducer作成中

--- src/pages/Basic.jsx
+++ src/pages/Basic.jsx
@@ -3,12 +3,18 @@ import { Link } from "react-router-dom";
 import { Gender } from "../components/Gender";
 import { BirthDate } from "../components/BirthDate";
 
-export const Basic = () => {
+import { connect } from "react-redux";
+
+export const Basic = ({ dispatch }) => {
   const [gender, setGender] = useState("男性");
 
   const [birthYear, setBirthYear] = useState();
   const [birthMonth, setBirthMonth] = useState();
   const [birthDay, setBirthDay] = useState();
+
+  const answerBasic = () => {
+    dispatch({ type: "ANSWERS", payload: gender });
+  }
 
   return (
     <>
@@ -18,6 +24,7 @@ export const Basic = () => {
       <Gender gender={gender} setGender={setGender} />
       <BirthDate birthYear={birthYear} setBirthYear={setBirthYear} birthMonth={birthMonth} setBirthMonth={setBirthMonth} birthDay={birthDay} setBirthDay={setBirthDay} />
       <br />
+      <button onClick={answerBasic}>更新</button>
       <div>
         <button><Link to="/question">次へ進む</Link></button>
       </div>
@@ -25,3 +32,14 @@ export const Basic = () => {
     
   )
 }
+
+const mapStateToProps = (state) => {
+  return {
+    gender: state.gender,
+    birthYear: state.birthYear,
+    birthMonth: state.birthMonth,
+    birthDay: state.birthDay,
+  }
+}
+
+export default connect(mapStateToProps)(Basic);
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import { createStore } from "redux";
+import { Basic } from '../pages/Basic';
+import { Question } from '../pages/Question';
+import { CounselingDetail } from '../pages/CounselingDetail';
 
 const initialState = {
   gender: "男性",
@@ -11,8 +14,22 @@ const initialState = {
   counselingDetail: "テストテストテストテストテスト",
 };
 
-const reducer = (state = initialState) => {
-  return state;
+const reducer = (state = initialState, action, payload) => {
+  switch (action.type) {
+    case 'ANSWERS':
+      return {
+        gender: state.gender = payload,
+        birthYear: state.birthYear = Basic.birthYear,
+        birthMonth: state.birthMonth = Basic.birthMonth,
+        birthDay: state.birthDay = Basic.birthDay,
+        question1: state.question1 = Question.question,
+        question2: state.question2 = Question.question2,
+        question3: state.question3 = Question.question3,
+        counselingDetail: state.counselingDetail = CounselingDetail,
+      }
+    default:
+      return state;
+  }
 };
 
 const store = createStore(reducer);
